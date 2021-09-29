4706aeb3bdbc7bdf334c7c06fe90b06e034f5f5b
leeyoong <nngeet010@naver.com>
2021-09-29 22:39:46 +0900

    프로그래머스 위클리 8주차 문제 (level 1)

---
+++ BaekJoon/위클리챌린지8주차.py
@@ -0,0 +1,9 @@
+def solution(sizes):
+    return max([max(size) for size in sizes]) * max([min(size) for size in sizes])
+
+# test case
+
+
+print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
+print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))
+print(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]))
